6badf087af932a9a2e10d210ef53f8d5b495080d
MagiX7 <DavidGonzalez2001-9@hotmail.com>
2020-11-05 18:48:09 +0100

    background collisions fixed, new ball?

--- Pinball/ModuleSceneIntro.cpp
+++ Pinball/ModuleSceneIntro.cpp
@@ -24,10 +24,10 @@ bool ModuleSceneIntro::Start()
 
 	App->renderer->camera.x = App->renderer->camera.y = 0;
 
-	circle = App->textures->Load("Assets/Assets/Ball2.png");
+	circle = App->textures->Load("Assets/Assets/ball4.png");
 	box = App->textures->Load("Assets/crate.png");
 	rick = App->textures->Load("Assets/rick_head.png");
-	background = App->textures->Load("Assets/Assets/background.png");
+	background = App->textures->Load("Assets/Assets/background2.png");
 
 	//App->physics->CreateChain(0, 0, bgPoints, 82, b2BodyType::b2_staticBody);
 	//App->physics->CreateChain(0, 0, upLeftPoints, 18, b2BodyType::b2_staticBody);
@@ -36,7 +36,7 @@ bool ModuleSceneIntro::Start()
 	//App->physics->CreateChain(0, 0, rightMiddlePoints, 28, b2BodyType::b2_staticBody);
 	//App->physics->CreateChain(0, 0, bottomRightPoints, 14, b2BodyType::b2_staticBody);
 
-	App->physics->CreateChain(0, 0, externBackground, 72, b2BodyType::b2_staticBody);
+	/*App->physics->CreateChain(0, 0, externBackground, 72, b2BodyType::b2_staticBody);
 	App->physics->CreateChain(0, 0, bottomRight, 20, b2BodyType::b2_staticBody);
 	App->physics->CreateChain(0, 0, bottomLeft, 22, b2BodyType::b2_staticBody);
 	App->physics->CreateChain(0, 0, bottomR, 16, b2BodyType::b2_staticBody);
@@ -44,11 +44,13 @@ bool ModuleSceneIntro::Start()
 	App->physics->CreateChain(0, 0, leftSide, 24, b2BodyType::b2_staticBody);
 	App->physics->CreateChain(0, 0, left, 10, b2BodyType::b2_staticBody);
 	App->physics->CreateChain(0, 0, rightSide2, 18, b2BodyType::b2_staticBody);
-	App->physics->CreateChain(0, 0, right, 10, b2BodyType::b2_staticBody);
+	App->physics->CreateChain(0, 0, right, 10, b2BodyType::b2_staticBody);*/
 	//App->physics->CreateChain(0, 0, externBackground2, 28, b2BodyType::b2_staticBody);
 	
+	App->physics->CreateChain(0, 0, bg, 112, b2_staticBody);
 
-	ballBody = App->physics->CreateCircle(162 * SCREEN_SIZE, 284 * SCREEN_SIZE, 13);
+
+	ballBody = App->physics->CreateCircle(472, 846, 15);
 
 
 	return ret;
@@ -75,31 +77,9 @@ update_status ModuleSceneIntro::Update()
 	// Draw Background
 	App->renderer->Blit(background, 0, 0, NULL);
 
+	App->renderer->Blit(circle, ballBody->GetPosition().x - 15, ballBody->GetPosition().y - 15, 0, 1.0f, ballBody->GetRotation());
 
-	App->renderer->Blit(circle, ballBody->GetPosition().x - 13, ballBody->GetPosition().y - 13, 0, 1.0f, ballBody->GetRotation());
-
-	// Draw Figures
-	p2List_item<PhysBody*>* c = circles.getFirst();
-	while (c != NULL)
-	{
-		// Offset takes the radius value
-		App->renderer->Blit(circle, c->data->GetPosition().x - 13, c->data->GetPosition().y - 13, 0, 1.0f, c->data->GetRotation());
-		c = c->next;
-	}
-
-	c = boxes.getFirst();
-	while (c != NULL)
-	{
-		App->renderer->Blit(box, c->data->GetPosition().x - 50, c->data->GetPosition().y - 25, 0, 1.0f, c->data->GetRotation());
-		c = c->next;
-	}
 	
-	c = ricks.getFirst();
-	while (c != NULL)
-	{
-		App->renderer->Blit(rick, c->data->GetPosition().x, c->data->GetPosition().y, 0, 1.0f, c->data->GetRotation());
-		c = c->next;
-	}
 
 	return UPDATE_CONTINUE;
 }
